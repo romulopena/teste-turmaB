7b1012121f336744dd3122f329858e1ef4cb134d
PROF ROMULO <rjlpena@gmail.com>
2023-10-05 16:56:04 -0300

    ok

--- main.js
+++ main.js
@@ -3,7 +3,3 @@ function tocaSomRisada(){
 }
 document.querySelector(".tecla_risada").onclick = tocaSomRisada;
 
-function tocaSomGrilo(){
-    document.querySelector("#som_tecla_grilo").play();
-}
-document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
