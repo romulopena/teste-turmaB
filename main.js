1e95ddae7ec0ce944ca056dadb4431f43c715d2a
PROF ROMULO <rjlpena@gmail.com>
2023-10-19 15:49:42 -0300

    lista

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tocaSomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+    document.querySelector('#som_tecla_aplausos').play();
 }
-document.querySelector(".tecla_aplausos").onclick = tocaSomAplausos;
+document.querySelector('.tecla_aplausos').onclick = tocaSomAplausos;
 
 const listaDeTeclas = querySelectorAll(".tecla");
 listaDeTeclas[0].onclick = tocaSomAplausos;
